6ea2328fdd5957fc829a166667d0d878e62206a5
mmarinavan70gmail.com <mmarinavan70>
2020-08-02 16:37:15 -0400

    Join again

---
+++ Melkonyan-3r/Team.java
@@ -0,0 +1,5 @@
+public class Melkonyan {
+	public static void main (String[] args){
+	System.out.println( "Team ");
+	}
+}
---
+++ TeamWork/Group-A.java
@@ -0,0 +1,5 @@
+Paclic class Group A {
+	public static void main( String [] args){
+	 System.out.println ( "Join group ");
+	}
+}
